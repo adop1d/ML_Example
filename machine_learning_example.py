4e4817552d29ea5eca13059f7136af79143efb7b
Kelvin Puch <kelpucha@gmail.com>
2025-09-11 18:25:26 -0400

    eerst commit, ML basics

---
+++ machine_learning_example.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import certifi
+import ssl
+from urllib.request import urlopen
+from sklearn.linear_model import LogisticRegression
+
+# Load the Iris dataset from a URL
+ssl_context = ssl.create_default_context(cafile=certifi.where())
+with urlopen('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
+            context=ssl_context) as response:
+      iris_data = pd.read_csv(response, header=None)
+# Assign column names to the dataset
+
+iris_data.columns = ['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm', 'Species']
+y = iris_data['Species']
+X = iris_data.drop(columns=['Species'])
+#split the data into features(x) and labels(y)
+print(X.head())
+
+model = LogisticRegression()
+model.fit(X.values, y)
+
+predictions = model.predict([[5.1, 3.5, 1.4, 0.2], [6.7, 3.1, 4.7, 1.5], [7.2, 3.6, 6.1, 2.5]])
+print(predictions)
+
+
+
