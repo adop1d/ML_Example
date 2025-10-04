0e5eddae92b841bf72101c2d25755130e294f6db
Kelvin Puch <kelpucha@gmail.com>
2025-10-04 13:58:54 -0400

    init dl ann example, imported necessary modules

---
+++ dl_artificial_neural_networks.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_circles
+from sklearn.neural_network import MLPClassifier
+import ipywidgets as widgets
+from IPython.display import display
+from ipywidgets import interactive
