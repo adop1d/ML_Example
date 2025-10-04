7e970f09929dc3328256fa09ec4bce988b979089
Kelvin Puch <kelpucha@gmail.com>
2025-10-04 14:23:14 -0400

    dl ann example done in a jupyterlab screen, idk why i didnt made it before like this XD

--- dl_artificial_neural_networks.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.datasets import make_circles
-from sklearn.neural_network import MLPClassifier
-import ipywidgets as widgets
-from IPython.display import display
-from ipywidgets import interactive
